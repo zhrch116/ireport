1f295f81def466365e97046b2ed144f0373cabeb
zhangruicheng <zhangruicheng@kyee.com.cn>
2017-04-05 19:23:53 +0800

    项目初始化

---
+++ hcrm-ireport/src/main/java/com/kyee/HcrmIreportApplication.java
@@ -0,0 +1,12 @@
+package com.kyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcrmIreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcrmIreportApplication.class, args);
+	}
+}
---
+++ hcrm-ireport/src/test/java/com/kyee/HcrmIreportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kyee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HcrmIreportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
